1b08e773a11f985c98095762a930d82e8a1dce8a
Andre <abrizidobasilio@gmail.com>
2023-04-20 10:57:21 -0300

    iniciando o jogo

---
+++ 2023-04-20/Jogo/Atributes.cs
@@ -0,0 +1,9 @@
+namespace Jogo
+{
+    public class Atributes
+    {
+        public int PontosDeVida = 20;
+        public int PontosDeDefesa = 0;
+        public int PontosDeAgilidade = 0;
+    }
+}
---
+++ 2023-04-20/POO/Program.cs
@@ -0,0 +1,28 @@
+﻿using _2023_04_20;
+
+
+Personagem tony = new Personagem();
+
+Console.WriteLine($"Digite o nome do personagem");
+tony.nome = Console.ReadLine();
+
+Console.WriteLine($"Digite a idade do personagem");
+tony.idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite a armadura do personagem");
+tony.armadura = Console.ReadLine();
+
+Console.WriteLine($"Digite a IA do personagem");
+tony.ia = Console.ReadLine();
+
+Console.WriteLine(@$"
+{tony.nome};
+{tony.idade};
+{tony.armadura};
+{tony.ia}.
+");
+
+tony.Atacar();
+Console.WriteLine(tony.Defender());
+tony.RestaurarArmadura();
+Console.WriteLine(tony.Aparar());
